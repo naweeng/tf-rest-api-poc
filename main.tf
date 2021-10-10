b61c1c8304f69266f99f7d78db7be8f162308928
Naween Ghimire <naween.g@joveo.com>
2021-10-10 13:44:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
-module "restapi" {
-  source  = "app.terraform.io/test-naween/restapi/provider"
-  version = "1.16.0"
+provider "restapi" {
+  uri = "https://rest.logentries.com"
+  debug = true
+  write_returns_object = true
+
+  headers = {
+    "x-api-key" = "f43010a1-d30e-4f82-9105-9c0492418a70",
+    "Content-Type" = "application/json"
+  }
+  create_method = "POST"
+  update_method = "PUT"
+  destroy_method = "DELETE"
+}
+
+resource "restapi_object" "tf_logset" {
+  path = "/management/logsets"
+  data = "{\"logset\":{\"name\":\"tf_logset_test_new_1\",\"user_data\":{}}}"
 }
